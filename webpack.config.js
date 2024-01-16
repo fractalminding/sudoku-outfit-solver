b31b35cb35ecef50749915c2b2c864a47c0175e6
Viktor Kozlov <menya.zovut@mail.ru>
2024-01-16 21:49:01 +0400

    webpack is working how i want! with my hard css-minifier

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const isProduction = process.env.NODE_ENV == 'production';
-
-
 const stylesHandler = 'style-loader';
-
 
 
 const config = {
@@ -27,7 +24,12 @@ const config = {
                 },
                 {
                     from: path.resolve(__dirname, 'src/assets.css'),
-                    to: path.resolve(__dirname, 'dist/assets.assets')
+                    to: path.resolve(__dirname, 'dist/assets.css'),
+                    transform: function(content) {
+                        return Buffer.from(
+                            content.toString().replace(/\s/g, ""), 'utf8'
+                        );
+                    }
                 }
             ],
           }),
